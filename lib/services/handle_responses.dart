03a55e6dc8f329f496e07467d19c3a845f266fc7
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-12 14:43:08 -0300

    refactor(handle_responses): change message

--- lib/services/handle_responses.dart
+++ lib/services/handle_responses.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
+/// Displays a SnackBar with a message based on the response code
 void handleResponse(Response response, BuildContext context) {
   if (response.statusCode == 200) {
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(
-        content: Text('Transacción agregada :v'),
+        content: Text('Operación exitosa :v'),
       ),
     );
   } else {
